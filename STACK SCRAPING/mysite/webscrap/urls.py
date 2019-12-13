14792987cb0b17d9061cbc721c93dded711b972c
sakib1486 <58849295+sakib1486@users.noreply.github.com>
2019-12-13 22:31:52 +0600

    Add files via upload

--- STACK SCRAPING/mysite/webscrap/urls.py
+++ STACK SCRAPING/mysite/webscrap/urls.py
@@ -4,5 +4,7 @@ from . import views
 #URL patterns
 urlpatterns = [
     path('webscrap/', views.index, name='index'),
-    path('contact/', views.contact, name='contact')
+    path('newest/', views.newest, name='newest'),
+    path('voted/', views.voted, name='voted'),
+    path('latest/', views.latest, name='latest')
 ]
--- STACK SCRAPING/mysite/webscrap/views.py
+++ STACK SCRAPING/mysite/webscrap/views.py
@@ -13,8 +13,11 @@ import bs4;
 def index(request) :
     return render(request, 'webscrap/home.html')
 
+def latest(request) :
+    return render(request, 'webscrap/latest.html')
 
-def contact(request) :
+
+def newest(request) :
     res = requests.get('https://stackoverflow.com/search?tab=newest&q=android%20features')  #Data from Stack Overflow
 
     soup = bs4.BeautifulSoup(res.text, 'lxml') #Building a lxml file from res.text
@@ -26,10 +29,31 @@ def contact(request) :
 
     for i in soup.select('.question-hyperlink'):
         ques[Count] = i.text
-        link[Count] = i.get('href')
+        link[Count] = "https://www.stackoverflow.com" + i.get('href')
         Count += 1
         if Count == 10:
             break
 
-    return render(request, 'webscrap/basic.html', {'res': zip(link, ques)})
-    #return render(request, 'webscrap/basic.html', {'ques' : [ques], 'link' : [link] })
+    return render(request, 'webscrap/newest.html', {'res': zip(link, ques)})
+    #return render(request, 'webscrap/newest.html', {'ques' : [ques], 'link' : [link] })
+
+
+#Most voted questions view
+def voted(request) :
+    res = requests.get('https://stackoverflow.com/search?tab=newest&q=android%20features')  #Data from Stack Overflow
+
+    soup = bs4.BeautifulSoup(res.text, 'lxml') #Building a lxml file from res.text
+
+    #building the question and hyperlink arrays
+    Count = 0
+    ques = [None] * 10
+    link = [None] * 10
+
+    for i in soup.select('.question-hyperlink'):
+        ques[Count] = i.text
+        link[Count] = "https://www.stackoverflow.com" + i.get('href')
+        Count += 1
+        if Count == 10:
+            break
+
+    return render(request, 'webscrap/voted.html', {'res': zip(link, ques)})
